327e2127c3935f730330402f381658421c6aa279
David Danielson <davidk.danielson@gmail.com>
2017-01-11 00:03:28 -0700

    worked on a closure toy problem

---
+++ closures-practice.js
@@ -0,0 +1,9 @@
+function showName(firstName, lastName){
+  var nameIntro = 'Your name is ';
+
+  function makeFullName(){
+    return nameIntro + firstName + ' ' + lastName;
+  }
+  return makeFullName();
+}
+showName('Davd', 'Danielson');
